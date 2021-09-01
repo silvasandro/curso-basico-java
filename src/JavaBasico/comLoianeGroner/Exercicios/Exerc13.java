ee343563cbee4f702a01183457c854f06d882c65
silvasandro <sandrosilva_2005@yahoo.com.br>
2021-08-31 20:24:43 -0400

    primeiro commit

--- src/JavaBasico/comLoianeGroner/Exercicios/Exerc13.java
+++ src/JavaBasico/comLoianeGroner/Exercicios/Exerc13.java
@@ -7,6 +7,15 @@ public class Exerc13 {
 
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Entre com uma letra (F ou M): ");
+        String input = scan.next();
 
+        if (input.equalsIgnoreCase("f")){
+            System.out.println("F - Feminino");
+        } else if (input.equalsIgnoreCase("m")){
+            System.out.println("M - Masculino");
+        } else {
+            System.out.println("Sexo Inválido");
+        }
     }
 }
