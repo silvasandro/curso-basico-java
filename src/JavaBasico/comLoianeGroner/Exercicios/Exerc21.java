f72afec2bc125eb85126a9e1ee596faea77cd9f7
silvasandro <sandrosilva_2005@yahoo.com.br>
2021-08-31 20:45:36 -0400

    Segundo commit - aula 21

--- src/JavaBasico/comLoianeGroner/Exercicios/Exerc21.java
+++ src/JavaBasico/comLoianeGroner/Exercicios/Exerc21.java
@@ -2,7 +2,7 @@ package JavaBasico.comLoianeGroner.Exercicios;
 
 import java.util.Scanner;
 
-public class Exerc13 {
+public class Exerc21 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
